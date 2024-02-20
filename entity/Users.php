6278cd8b5464b2366cbe12d1b37a53dc9e8f20fe
avilovmaksim5 <avilovmaksim5@mail.ru>
2024-02-20 19:40:27 +0400

    Initial commit

---
+++ entity/Users.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace app\entity;
+
+use Yii;
+
+/**
+ * This is the model class for table "user".
+ *
+ * @property int $id
+ * @property string $email
+ * @property string $password
+ * @property string $name
+ *
+ * @property Messages[] $messages
+ * @property Topic[] $topics
+ */
+class Users extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'user';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['email', 'password', 'name'], 'required'],
+            [['name'], 'string'],
+            [['email'], 'email'],
+            [['email', 'password'], 'string', 'max' => 100],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'email' => 'Email',
+            'password' => 'Password',
+            'name' => 'Name',
+        ];
+    }
+
+    /**
+     * Gets query for [[Messages]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getMessages()
+    {
+        return $this->hasMany(Messages::class, ['user_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[Topics]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getTopics()
+    {
+        return $this->hasMany(Topic::class, ['user_id' => 'id']);
+    }
+}
